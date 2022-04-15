f6c5ffc6a71a09932ca6c56f074ab60e7b6663ba
Sunny <sunnygb.8@gmail.com>
2022-04-15 09:11:22 +0800

    update scripts

---
+++ .devcontainer/scripts/install-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# - execute this at root - bash ./.devcontainer/scripts/install-packages.sh
+# - go to target project
+# cd src/my-nextapp
+cd $1
+# - packages, libraries (comment or uncomment libraries as needed)
+# - for parsing markdown files
+# yarn add gray-matter
+# - render markdown
+# yarn add remark remark-html
+# - format date
+# yarn add date-fns
--- .devcontainer/scripts/new-app.sh
+++ .devcontainer/scripts/new-app.sh
@@ -1,6 +1,8 @@
-# npx create-next-app@latest
+# Prepare a next project
+# - execute this at root - bash ./.devcontainer/scripts/new-app.sh
+cd src
 npx create-next-app@latest --typescript
 
-
+# npx create-next-app@latest
 # yarn create next-app
 # yarn create next-app --typescript
